ec09830c62fb8e3441b063f0aba8c4b3f07a3540
didi350 <dimitrova.desanka@gmail.com>
2026-02-28 16:21:39 +0100

    Update init_database.sql
    Signed-off-by: didi350 <dimitrova.desanka@gmail.com>

--- Scripts/init_database.sql
+++ Scripts/init_database.sql
@@ -1,6 +1,16 @@
--- Create brand new database called "Data Warehouse Project".
--- In order to do that, first we have to switch to the "master" database.
--- This is a system database in SQL Server where you can go and create other databases.
+/*
+Create Database and Schemas
+===========================
+Script Purpose:
+This script creates a new database named "Data Warehouse Project" after checking if it already exists.
+If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas
+within the database: "bronze", "silver", and "gold", following our architecture design.
+
+WARNING:
+Running this script will drop the entire "Data Warehouse Project" database if it exists.
+All data in the database will be permanently deleted. Proceed with caution
+and ensure you have proper backups before running this script.
+*/
 
 USE master; -- Switch to the master database (system) to create your new project database.
 
@@ -15,10 +25,9 @@ CREATE DATABASE "Data Warehouse Project";
 
 USE "Data Warehouse Project"; -- Switch to the new database. 
 
--- Now we can go and start building stuff inside this database.
--- Next step is to start creating the schemas.
--- So what is a schema? Think about it like a folder or a container that helps you to keep things organized.
--- So as we decided in the architecture, we have three layers: bronze, silver, and gold. 
+-- Now we can go and start building things inside this database.
+-- Next step is to start creating the schemas. The meaning of schema is, you can think about it like a folder or a container that helps you to keep things organized.
+-- So as we decided in the architecture, we have three layers: bronze, silver, and gold.
 -- And now we're going to create a schema for each layer.
 
 CREATE SCHEMA bronze;
